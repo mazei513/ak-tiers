beeaf6f19dd5433a605d3ae70cbdcbce75980b0f
Azraei Yusof <mazei513@gmail.com>
2024-10-20 19:09:14 +0800

    Fixed service worker

--- sw.js
+++ sw.js
@@ -1,34 +1,43 @@
 const cacheName = "v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(async () => {
-    const cache = await caches.open(cacheName);
-    await cache.add("./");
-  });
-});
 
+self.addEventListener("install", (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.add(new Request("./", { cache: "reload" }));
+  })())
+});
 self.addEventListener("activate", (e) => {
   e.waitUntil(self.registration?.navigationPreload.enable());
 });
-
 self.addEventListener("fetch", (e) => {
-  e.respondWith(async () => {
-    const cacheResp = await caches.match(e.request);
-    if (cacheResp) return cacheResp;
-
-    const preloadResp = await e.preloadResponse;
-    if (preloadResp) {
-      putInCache(e.request, preloadResp);
-      return preloadResp;
-    }
-
-
-    const netResp = fetch(e.request);
-    putInCache(e.request, netResp);
-    return netResp;
-  });
+  e.respondWith(fetchHandler(e));
 });
 
-async function newFunction(req, resp) {
+async function fetchHandler(e) {
+  const cache = await caches.open(cacheName)
+  const cacheResp = await cache.match(e.request);
+  if (cacheResp) {
+    netResp(e).then((netResp) => {
+      putInCache(e.request, netResp);
+    });
+    return cacheResp;
+  }
+  const resp = await netResp(e);
+  putInCache(e.request, resp)
+  return rest;
+}
+
+async function netResp(e) {
+  const preloadResp = await e.preloadResponse;
+  if (preloadResp) {
+    return preloadResp;
+  }
+
+  const netResp = await fetch(e.request);
+  return netResp;
+}
+
+async function putInCache(req, resp) {
   const cache = await caches.open(cacheName)
   await cache.put(req, resp.clone());
 }
